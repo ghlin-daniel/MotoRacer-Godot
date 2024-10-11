89803bd6fa447266e0a76bb34d909dc454f80a19
Daniel Lin <ghlin.daniel@gmail.com>
2024-10-11 09:06:16 -0700

    Added game logic

--- main.gd
+++ main.gd
@@ -1,13 +1,39 @@
 extends Node
 
-func new_game() -> void:
-	$Racer.start($StartPosition.position)
+@export var car_scene: PackedScene
+var score
 
-# Called when the node enters the scene tree for the first time.
+func new_game():
+	score = 0
+	$Racer.start($StartPosition.position)
+	$StartTimer.start()
+
 func _ready() -> void:
 	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: float):
 	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$CarTimer.stop()
+
+func _on_score_timer_timeout():
+	score += 1
+
+func _on_start_timer_timeout():
+	$CarTimer.start()
+	$ScoreTimer.start()
+
+func _on_car_timer_timeout():
+	var car = car_scene.instantiate()
+	
+	var car_spawn_location = $CarPath/CarSpawnLocation
+	car_spawn_location.progress_ratio = randf()
+
+	car.position = car_spawn_location.position
+	
+	var velocity = Vector2(0, randf_range(150, 250))
+	car.linear_velocity = velocity
+	
+	add_child(car)
--- racer.gd
+++ racer.gd
@@ -7,16 +7,14 @@ var right_bottom_bound
 
 func start(pos):
 	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	var screen_size = get_viewport_rect().size
 	right_bottom_bound= Vector2(screen_size.x - 30, screen_size.y)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: float):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -27,5 +25,7 @@ func _process(delta: float) -> void:
 	position = position.clamp(left_top_bound, right_bottom_bound)
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D):
+	hide()
 	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
